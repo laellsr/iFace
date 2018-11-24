a0bc2d35aa2fab37e28763a4903a988e208612f2
laellsr <lael_lsr@hotmail.com>
2018-11-24 15:35:49 -0300

    developing

--- src/iFace/Main.java
+++ src/iFace/Main.java
@@ -10,7 +10,7 @@ public class Main
 	{
 		ArrayList <SingUp> accounts = new ArrayList<SingUp>();
 		Scanner input = new Scanner(System.in);
-		int choise, index=0; String username = new String(), password = new String();
+		int choise, user_choise, index=0; String username = new String(), password = new String();
 		boolean active = true;
 		
 		while(active)
@@ -53,8 +53,56 @@ public class Main
 					
 					while(case1)
 					{
-						System.out.printf("\nOlá, %s\n, este é o seu Perfil\n", accounts.get(index).getName());
+						System.out.printf("\nOlá, %s, este é o seu Perfil\n", accounts.get(index).getName());
+						System.out.printf("%s, %s, ", accounts.get(index).getGender(), accounts.get(index).getAge());
+						System.out.printf("\"%s\"\n%s\n%s\n", accounts.get(index).getPhrase(),
+								accounts.get(index).getMore1(), accounts.get(index).getMore2());
+						
+						System.out.printf("\n[1] Editar perfil\n[2] Adicionar amigo\n[3] Enviar mensagem\n"
+								+ "[4] Criar comunidade\n[5] Entrar em comunidade\n[6] Recuperar dados\n[7] Excluir conta\n"
+								+ "[8] Sair\n=> ");
+						
+						user_choise = input.nextInt();
+						input.nextLine();
+						
+						switch(user_choise)
+						{
+							case 1:
+								accounts.get(index).setName();
+								accounts.get(index).setGender();
+								accounts.get(index).setAge();
+								accounts.get(index).setPhrase();
+								accounts.get(index).setMore1();
+								accounts.get(index).setMore2();
+								break;
+								
+							case 2:
+								break;
+								
+							case 3:
+								break;
+								
+							case 4:
+								break;
+								
+							case 5:
+								break;
+								
+							case 6:
+								break;
+								
+							case 7:
+								break;
+								
+							case 8:
+								case1 = false;
+								break;
+								
+							default:
+								break;
+						}
 					}
+					break;
 		
 				
 				case 2:
--- src/iFace/SingUp.java
+++ src/iFace/SingUp.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class SingUp
 {
-	String username, name, password;
+	String username, password;
+	String name, age = "-----", gender = "-----", phrase = "-----", more1 = "-----", more2 = "-----";
 	Scanner input = new Scanner(System.in);
 	public void newUser(String username)
 	{
@@ -26,8 +27,74 @@ public class SingUp
 		return password;
 	}
 	
+	public void setPassword()
+	{
+		System.out.printf("\nDigite sua nova senha:\n=> ");
+		password = input.nextLine();
+	}
+	
 	public String getName()
 	{
 		return name;
+	}
+	
+	public void setName()
+	{
+		System.out.printf("\nDigite seu novo nome de perfil:\n=> ");
+		name = input.nextLine();
+	}
+	
+	public String getAge()
+	{
+		return age;
+	}
+	
+	public void setAge()
+	{
+		System.out.printf("\nDigite sua idade:\n=> ");
+		age = input.nextLine();
+	}
+	
+	public String getGender()
+	{
+		return gender;
+	}
+	
+	public void setGender()
+	{
+		System.out.printf("\nDigite seu gênero:\n=> ");
+		gender = input.nextLine();
+	}
+	
+	public String getPhrase()
+	{
+		return phrase;
+	}
+	
+	public void setPhrase()
+	{
+		System.out.printf("\nDigite uma frase favorita:\n=> ");
+		phrase = input.nextLine();
+	}
+	public String getMore1()
+	{
+		return more1;
+	}
+	
+	public void setMore1()
+	{
+		System.out.printf("\nAdicione outra informação sobre você\n=> ");
+		more1 = input.nextLine();
+	}
+	
+	public String getMore2()
+	{
+		return more2;
+	}
+	
+	public void setMore2()
+	{
+		System.out.printf("\nAdicione outra informação sobre você\n=> ");
+		more2 = input.nextLine();
 	}
 }
